e2e4ea1b08226020a38c7ec35c380a1c5a3c81e6
theRealBassist <rangercam98@gmail.com>
2024-06-11 11:44:21 -0400

    modified:   Lesson 2/Lesson2.py

--- Lesson 2/Lesson2.py
+++ Lesson 2/Lesson2.py
@@ -22,11 +22,11 @@ print(myInt + 1)
 # The int() method can also be used to convert a float into an integer.
 # When this is done, Python will "floor" or round-down the number to the nearest integer
 
-print(int(6.98273))
+print(int(6.98273)) # This will return 6
 
 # The int() method can also be used to convert boolean True/False values to 1 or 0, respectively.
 
-print(int(True))
+print(int(True)) # This will return 1
 
 
 ## float() ##
@@ -36,6 +36,11 @@ myFloat = float(input("Please enter a Float: "))
 print(myFloat)
 
 ## str() ##
+
+# It just converts whatever to a string
+
+myString = str(89.6154) + "test"
+print(myString)
 
 
 
@@ -68,6 +73,55 @@ print(myString[2]) # This will print "e" because it is at the index 1.
 # In the previous string the character at index -1 is "o"
 
 print (myString[-1])
+
+# You can also indicate any characters between two values by using the ":" character
+# It will include the value to the left of the ":" and stop at the index one less than the value to the right
+
+print (myString[1:3]) # This will print "el" as those character have the indices of 1, 2, and 3
+print (myString[:2]) # This will print "He" because a blank before the ":" means we start at 0
+print (myString[3:]) # this will print "lo" because a blank after the ":" means we end at -1
+
+## String Methods ##
+
+# count(value) # Returns the number of times a specified value occurs in a string
+testString = "hello"
+testString.count("l") # returns 2
+
+# endswith(value) # Returns true if the string ends with the specified value
+testString = "myFile.txt"
+testString.endswith(".txt") # returns True
+
+# find(value) # Searches the string for a specified value and returns the position of where it was found
+testString = "myFile.txt"
+testString.find("File") # returns 2
+
+# index() # Searches the string for a specified value and returns the position of where it was found
+testString = "myFile.txt"
+testString.index(".") # returns 6
+
+# len() # Returns the number of characters in a string
+testString = "test"
+len(testString) # returns 4
+
+# lower() # Converts a string into lower case
+testString = "TEST"
+testString.lower() # returns "test"
+
+# strip() # Returns a trimmed version of the string
+testString = "   test   "
+testString.strip() # returns "test"
+
+# replace() # Returns a string where a specified value is replaced with a specified value
+testString = "table"
+testString.replace("able", "est") # returns "test"
+
+# startswith() # Returns true if the string starts with the specified value
+testString = "myFile.txt"
+testString.startswith("myFile") # returns True
+
+# upper() # Converts a string into upper case
+testString = "test"
+testString.upper() # returns "TEST"
 
 ## F Strings ##
 
